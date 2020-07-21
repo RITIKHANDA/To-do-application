9e434188e387a5799535602886963a6fb329d66a
RITIKHANDA <ritik.handa98@gmail.com>
2020-07-22 04:02:47 +0530

    1st commit

---
+++ src/Todomain.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.text.ParseException;
+
+public class Todomain
+{
+	
+	
+	public static void main(String[] args) throws IOException, ParseException
+	{
+
+
+		Taskinput dooption = new Taskinput();
+		dooption.displayOption();
+	}
+}
